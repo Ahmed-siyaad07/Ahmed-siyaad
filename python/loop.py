e2d74979a4092370c8ce8ae7b83c6284292b0539
Ahmed siyaad <waasugesiyaad313@gmail.com>
2024-11-18 12:14:29 +0300

    Add files via upload

---
+++ python/loop.py
@@ -0,0 +1,54 @@
+COUNT = 1
+# while COUNT < 11:
+#     print(COUNT)
+#     COUNT = COUNT +1
+
+# COUNT = 0
+# while COUNT < 3:
+#     print('HELLO THE WORLD')
+#     COUNT += 1
+
+# ANS = input("ENTER 'Y' OR 'N' ")
+# while ANS == 'Y':
+#     print('THANKS')
+#     ANS = input("ENTER 'Y' OR 'Y'")
+
+# NUM = int(input("START"))
+# COUNT = 0
+# while COUNT <10:
+#     print(NUM)
+#     COUNT += 1
+#     NUM += 1
+
+
+# for I in range(1000,0,-1):
+#  print(I)
+# START = int(input('START'))
+# END = int(input('END'))
+# STEP = int(input('STEP'))
+# for I in range(START,END,STEP):
+#     print(I)
+
+# ORI = float(input('SOO GELI QIIIMAHIISA'))
+# DIC = ORI*0.2
+# SALES = ORI-DIC
+# print("THE SALES PRICE" , SALES)
+# START = int(input('START :'))
+# END = int(input('END: '))
+# TOTAL = (START+END)
+# for I in range(START,END):
+#     print(TOTAL)
+
+# T1= float(input(' SOOGELI'))
+# T2 = float(input(' SOO GELI '))
+# T3 = float(input('SOO GELI'))
+# AVERAGE = (T1 + T2 + T3 / 3)
+# print(AVERAGE)
+
+TEST1 = int(input('SOO GELI NA 1 '))
+TEST2 = int(input('SOO GELI NA2 '))
+TEST3 = int(input(' SOO GELI NA3  '))
+AVERAGE = (TEST1 + TEST2 + TEST3 )/3
+print(AVERAGE)
+if AVERAGE >=90:
+    print('HAMBALYO')
